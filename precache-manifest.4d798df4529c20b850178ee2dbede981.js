b20407d1534279a4348b9ed14d4d58fd1a3b377d
thearturord <47672781+thearturord@users.noreply.github.com>
2022-08-03 19:42:03 +0200

    .

---
+++ precache-manifest.4d798df4529c20b850178ee2dbede981.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f80ff0756cb0e604dcf",
+    "url": "/static/css/main.fa5b499d.chunk.css"
+  },
+  {
+    "revision": "8f80ff0756cb0e604dcf",
+    "url": "/static/js/main.8f80ff07.chunk.js"
+  },
+  {
+    "revision": "8db5205d0a8c24248ce6",
+    "url": "/static/js/1.8db5205d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0def0cd581f2971f00eafec9c98636d",
+    "url": "/index.html"
+  }
+];
